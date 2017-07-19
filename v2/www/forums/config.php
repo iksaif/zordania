74621bd9e11e0d37ad7d3b39bf7f5497ff9d24ae
BehoDouble <behodouble@gmail.com>
2017-07-19 22:50:21 +0200

    Modification de la config du forum PunBB pour utiliser mysqli.

--- v2/www/forums/config.php
+++ v2/www/forums/config.php
@@ -3,7 +3,7 @@
 require_once("../../conf/conf.inc.php");
 require_once("../../lib/divers.lib.php");
 
-$db_type = 'mysql';
+$db_type = 'mysqli';
 $db_host = MYSQL_HOST;
 $db_name = MYSQL_BASE;
 $db_username = MYSQL_USER;
